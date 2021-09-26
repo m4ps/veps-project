42024a298727150553b0ebefcb49b70fa04f480d
pavel matveev <pavel.m@ati.su>
2021-09-26 14:05:22 +0300

    Initial Commit

---
+++ Shared/veps_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  veps_projectApp.swift
+//  Shared
+//
+//  Created by Павел Матвеев on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct veps_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
